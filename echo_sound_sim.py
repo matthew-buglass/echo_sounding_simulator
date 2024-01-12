2e0d46349bdbcf3efbe7c9eb83e5c0a1df3e4aa6
matthew-buglass <buglassmatthew@gmail.com>
2024-01-11 22:27:28 -0600

    wrote code and updated the test mesh for finding the appropriate depth reading at an [x y] position. Still need to write tests

--- echo_sound_sim.py
+++ echo_sound_sim.py
@@ -1,9 +1,8 @@
 import sys
-import numpy as np
 from trimesh import load, Trimesh
 
 from utils.cli_parsing import parse_args
-from utils.geometry import point_in_tri
+from utils.geometry import point_in_tri, triangular_plane_intercept
 
 
 def find_shallowest_depth(mesh: Trimesh, x: float, y:float):
@@ -15,13 +14,20 @@ def find_shallowest_depth(mesh: Trimesh, x: float, y:float):
     :param y: a real y position
     :return: a real number that is the z position closest to 0
     """
+    # Pick the lowest point in the mesh as our starting max
+    max_z = mesh.bounds[0, 2]
+
     for i, face in enumerate(mesh.faces):
         v1 = mesh.vertices[face[0]]
         v2 = mesh.vertices[face[1]]
         v3 = mesh.vertices[face[2]]
 
         if point_in_tri((x, y), v1, v2, v3):
-            print(i, v1, v2, v3)
+            z = triangular_plane_intercept(x, y, v1, v2, v3)
+            if z > max_z:
+                max_z = z
+
+    return max_z
 
 
 if __name__ == '__main__':
@@ -30,12 +36,12 @@ if __name__ == '__main__':
 
     # Import data file
     mesh = load(args.data_file)
-    min_x, min_y, min_z = mesh.bounds[0]
-    max_x, max_y, max_z = mesh.bounds[1]
+    min_x, min_y, _ = mesh.bounds[0]
+    max_x, max_y, _ = mesh.bounds[1]
 
-    print(f"Min Bounds {min_x} {min_y} {min_z}")
-    print(f"Min Bounds {max_x} {max_y} {max_z}")
-    print(f"Point samples {mesh.sample(3)}")
+    print(f"Min Bounds {min_x} {min_y}")
+    print(f"Min Bounds {max_x} {max_y}")
+    print(f"Point samples \n{mesh.sample(3)}")
 
     # find a face that contains a point
-    find_shallowest_depth(mesh, 0, 0)
+    print(f"z at origin {find_shallowest_depth(mesh, 0, 0)}")
